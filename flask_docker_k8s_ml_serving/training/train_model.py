ae45da88503196924807d0ee611e9f9caa6ac04d
Ekhtiar Syed <ekhtiar.syed@signify.com>
2020-09-10 23:39:36 +0200

    moving flask_docker.. to folders

--- flask_docker_k8s_ml_serving/training/train_model.py
+++ flask_docker_k8s_ml_serving/training/train_model.py
@@ -1,6 +1,7 @@
 # Trained model 
 from sklearn import datasets
 import pandas as pd
+import pickle
 from sklearn import tree
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
@@ -22,6 +23,5 @@ clf = clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 print("accuracy_score: %.2f" % accuracy_score(y_test, y_pred))
 
-# save the model in a pickle file 
-import pickle
+# save the model in a pickle file
 pickle.dump(clf, open('prediction.pickle', 'wb'))
