9fa856a2071df295935edb868f4396bdf9c2d860
Ekhtiar Syed <ekhtiar.syed@signify.com>
2020-09-11 00:30:04 +0200

    automating creation and upload to gcs

--- flask_docker_k8s_ml_serving/train/train_model.py
+++ flask_docker_k8s_ml_serving/train/train_model.py
@@ -5,6 +5,7 @@ import pickle
 from sklearn import tree
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from google.cloud import storage
 
 # import the iris dataset
 iris = datasets.load_iris()
@@ -24,4 +25,16 @@ y_pred = clf.predict(X_test)
 print("accuracy_score: %.2f" % accuracy_score(y_test, y_pred))
 
 # save the model in a pickle file
-pickle.dump(clf, open('prediction.pickle', 'wb'))
+pickle.dump(clf, open('./prediction.pickle', 'wb'))
+
+bucket_name = "flower-prediction"
+source_file_name = "./prediction.pickle"
+destination_blob_name = "prediction.pickle"
+
+storage_client = storage.Client()
+bucket = storage_client.create_bucket(bucket_name)
+print("Bucket {} created".format(bucket.name))
+blob = bucket.blob(destination_blob_name)
+blob.upload_from_filename(source_file_name)
+print("File {} uploaded to {}.".format(source_file_name, destination_blob_name))
+
