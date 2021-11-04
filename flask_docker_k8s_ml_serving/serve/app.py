c633737a00fbc60ced0d88f900592063e89be612
Fanus Arefaine <fanus.gebrehiwet@gmail.com>
2021-11-03 23:57:39 -0700

    Bucket name conversion

--- flask_docker_k8s_ml_serving/serve/app.py
+++ flask_docker_k8s_ml_serving/serve/app.py
@@ -7,29 +7,32 @@ from google.cloud import storage
 app = Flask(__name__)
 
 # when the user sends a POST request
+
+
 @app.route('/api', methods=['POST'])
 def makecalc():
     # get the data from the user
     data = request.get_json()
     data = np.array(data).reshape(1, -1)
-    
+
     # uses the model and the input data to predict
     pred = model.predict(data)
     prediction = np.array2string(pred)
     return jsonify(prediction)
 
+
 if __name__ == '__main__':
-    # to download from Google Cloud Storage the pickle file 
+    # to download from Google Cloud Storage the pickle file
     storage_client = storage.Client()
-    bucket = storage_client.bucket('flower_predictions')
+    bucket = storage_client.bucket('fp_farefain')
     # loads the pickle file
     blob = bucket.blob('prediction.pickle')
-    # saves temporarily the pickle file 
+    # saves temporarily the pickle file
     temp_model_location = './temp_model.pickle'
     blob.download_to_filename(temp_model_location)
     with open(temp_model_location, "rb") as f:
         model = p.load(f)
-        
+
     # runs the app
     # to run the app locally use host='127.0.0.1'
     app.run(debug=False, host='0.0.0.0')
